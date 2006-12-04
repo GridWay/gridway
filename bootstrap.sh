8407feea67a0327b79438a09d017800216ad104d
ruben <ruben>
2006-12-04 16:05:35 +0000

    [project @ 2006-12-04 16:05:35 by ruben] Autotools debugging
    git-svn-id: http://svn.gridway.org/gridway/trunk@52 9b4b2aa8-92f0-458a-8103-c3834d721760

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
+if [ ! -d config ] ; then
+    mkdir config
+    autoscan
+    touch makefile-header
+fi    
+
 libtoolize --force 2>&1 \
 && aclocal  2>&1 \
 && autoheader 2>&1 \
-&& automake -i --gnu --add-missing 2>&1 \
+&& automake -i --foreign --copy --add-missing 2>&1 \
 && autoconf  2>&1
