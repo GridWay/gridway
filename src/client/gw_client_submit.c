dc6261f7566fb15de4897088eb86d44af087862e
ehuedo <ehuedo>
2009-02-11 16:24:30 +0000

    [project @ 2009-02-11 16:24:30 by ehuedo] Minor change for Bug 6624 (Campaign): Support for multiple proxies per user
    git-svn-id: http://svn.gridway.org/gridway/trunk@170 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/client/gw_client_submit.c
+++ src/client/gw_client_submit.c
@@ -102,7 +102,7 @@ gw_return_code_t gw_client_job_submit(char *         template,
 	
 	strncpy(msg.owner,gw_client.owner,GW_MSG_STRING_SHORT);
 	strncpy(msg.group,gw_client.group,GW_MSG_STRING_SHORT);
-	strncpy(msg.proxy_path,gw_client.proxy_path,GW_MSG_STRING_SHORT);
+	strncpy(msg.proxy_path,gw_client.proxy_path,GW_MSG_STRING_LONG);
 	
 	pthread_mutex_unlock(&(gw_client.mutex));	
 	
@@ -256,7 +256,7 @@ gw_return_code_t gw_client_array_submit(char *         template,
 	
 	strncpy(msg.owner,gw_client.owner,GW_MSG_STRING_SHORT);
 	strncpy(msg.group,gw_client.group,GW_MSG_STRING_SHORT);
-	strncpy(msg.proxy_path,gw_client.proxy_path,GW_MSG_STRING_SHORT);
+	strncpy(msg.proxy_path,gw_client.proxy_path,GW_MSG_STRING_LONG);
 		
 	pthread_mutex_unlock(&(gw_client.mutex));
 	
