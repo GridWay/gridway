ec78ad67ce47e488612ff612c4275ef67858ac4a
ehuedo <ehuedo>
2009-11-04 17:29:28 +0000

    Proper initialization of proxy_path (issue #71).
    git-svn-id: http://svn.gridway.org/gridway/trunk@193 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/client/gw_client_submit.c
+++ src/client/gw_client_submit.c
@@ -261,7 +261,11 @@ gw_return_code_t gw_client_array_submit(char *         template,
 	
 	strncpy(msg.owner,gw_client.owner,GW_MSG_STRING_SHORT);
 	strncpy(msg.group,gw_client.group,GW_MSG_STRING_SHORT);
-	strncpy(msg.proxy_path,gw_client.proxy_path,GW_MSG_STRING_LONG);
+
+        if (gw_client.proxy_path == NULL)
+            msg.proxy_path[0] = '\0';
+        else
+	    strncpy(msg.proxy_path,gw_client.proxy_path,GW_MSG_STRING_LONG);
 		
 	pthread_mutex_unlock(&(gw_client.mutex));
 	
