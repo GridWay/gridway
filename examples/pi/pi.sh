7e01ee364e38394aba06525efdc6057a0576f2b2
alorca <alorca>
2010-02-03 15:01:29 +0000

    Pi example improved by compiling the source code in the working node.
    git-svn-id: http://svn.gridway.org/gridway/trunk@220 9b4b2aa8-92f0-458a-8103-c3834d721760

--- examples/pi/pi.sh
+++ examples/pi/pi.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-
+# Submitting the 4 jobs to GridWay and getting the Array Identificator
 AID=`gwsubmit -v -t pi.jt -n 4 | head -1 | awk '{print $3}'`
 
-if [ $? -ne 0 ]
-then
-    echo "Submission failed!"
-    exit 1
+# If succeeded we keep further otherwise inform the user
+if [ $? -ne 0 ]; then
+	echo "Submission failed!"
+	exit 1
 fi
 
+# It is time to wait for the collection of 4 jobs to finish, so the user does not receive the shell prompt
 gwwait -v -A $AID
 
+# Once the waiting is over, compute the sum of the stdout_files
 if [ $? -eq 0 ]
 then
-    awk 'BEGIN {sum=0} {sum+=$1} END {printf "Pi is %0.12g\n", sum}' stdout_file.*
+	awk 'BEGIN {sum=0} {sum+=$1} END {printf "Pi is %0.10f\n", sum}' stdout_file.*
 else
-    echo "Some tasks failed!"
+	echo "Some tasks failed! Check the errors with gwhistory or looking to the $GW_LOCATION/var/\$JOB_ID/job.log"
 fi
-
-gwkill -A $AID
