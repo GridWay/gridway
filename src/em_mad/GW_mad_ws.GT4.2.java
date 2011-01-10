088d26abbcfdde25e47b9e053871ce9771016e5f
ehuedo <ehuedo>
2011-01-10 14:52:19 +0000

    Bug #86: Problems with java.util.Calendar
    git-svn-id: http://svn.gridway.org/gridway/trunk@247 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/em_mad/GW_mad_ws.GT4.2.java
+++ src/em_mad/GW_mad_ws.GT4.2.java
@@ -489,18 +489,17 @@ class Service extends Thread {
         }
 
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.WEEK_OF_YEAR , calendar.get(Calendar.WEEK_OF_YEAR) + 1);
+        calendar.add(Calendar.WEEK_OF_YEAR, 1);
         
 	try
 	{ 
-        	job.setTerminationTime(calendar.getTime()); // One week
+            job.setTerminationTime(calendar.getTime()); // One week
 	}
 	catch (Exception e)
 	{
 	}
 
         // Submit the job
-        
         try
         {
             job.submit(factoryEndpoint, false, false, null);
