3277b5978a5d7dda8451b3265a957d95d0bf4e23
imarin <imarin>
2011-03-21 16:15:10 +0000

    Changes in classpath
    git-svn-id: http://svn.gridway.org/gridway/branches/bes_branch@332 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/em_mad/OGSA-BES/gw_em_mad_bes.sh
+++ src/em_mad/OGSA-BES/gw_em_mad_bes.sh
@@ -15,4 +15,4 @@ cd_var
 mad_debug
 check_proxy
 
-exec nice -n $PRIORITY java -classpath $GW_LOCATION/lib/gw_em_mad_bes.jar:$JAR_FILES/axis.jar:$JAR_FILES/jaxrpc.jar:$GRIDSAM_HOME/WEB-INF/lib/gridsam-schema-2.3.0.jar:$JAR_FILES/xmlbeans.jar:$JAR_FILES/commons-logging.jar:$JAR_FILES/commons-discovery.jar:/home/imarin/axis-1_4/lib/mailapi_1_3_1.jar:$GRIDSAM_HOME/WEB-INF/lib/gridsam-core-2.3.0.jar:$JAR_FILES/wsdl4j.jar:$JAR_FILES/commons-httpclient.jar:$JAR_FILES/commons-codec.jar:$JAR_FILES/dom4j.jar -Djava.endorsed.dirs=$GW_LOCATION/lib/endorsed -Daxis.ClientConfigFile=$GW_LOCATION/lib/client-config.wsdd GW_em_mad_bes 
+exec nice -n $PRIORITY java -classpath $GW_LOCATION/lib/gw_em_mad_bes.jar:$JAR_FILES/axis.jar:$JAR_FILES/jaxrpc.jar:$GRIDSAM_HOME/WEB-INF/lib/gridsam-schema-2.3.0.jar:$JAR_FILES/xmlbeans.jar:$JAR_FILES/commons-logging.jar:$JAR_FILES/commons-discovery.jar:$GRIDSAM_HOME/WEB-INF/lib/gridsam-core-2.3.0.jar:$JAR_FILES/wsdl4j.jar:$JAR_FILES/commons-httpclient.jar:$JAR_FILES/commons-codec.jar:$JAR_FILES/dom4j.jar -Djava.endorsed.dirs=$GW_LOCATION/lib/endorsed -Daxis.ClientConfigFile=$GW_LOCATION/lib/client-config.wsdd GW_em_mad_bes 
