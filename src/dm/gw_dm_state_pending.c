44c5c6fdacae713f045b5ff4061031e86e04445e
ehuedo <ehuedo>
2011-08-17 09:09:05 +0000

    Aesthetic changes
    git-svn-id: http://svn.gridway.org/gridway/trunk@429 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/dm/gw_dm_state_pending.c
+++ src/dm/gw_dm_state_pending.c
@@ -23,33 +23,33 @@
 #include "gw_dm.h"
 #include "gw_user_pool.h"
 
-void gw_dm_pending ( void *_job_id )
+void gw_dm_pending( void *_job_id )
 {
-    gw_job_t *            job;
-    int                   job_id;
-    gw_boolean_t          failed;
+    gw_job_t *job;
+    int job_id;
+    gw_boolean_t failed;
     gw_migration_reason_t reason;
     
-	/* ----------------------------------------------------------- */  
+    /* ----------------------------------------------------------- */  
     /* 0.- Get job pointer                                         */
     /* ----------------------------------------------------------- */  
     
-	if ( _job_id != NULL )
-	{
-		job_id = *( (int *) _job_id );
+    if ( _job_id != NULL )
+    {
+        job_id = *( (int *) _job_id );
 
-		job = gw_job_pool_get(job_id, GW_TRUE);
+        job = gw_job_pool_get(job_id, GW_TRUE);
 
-		if ( job == NULL )
-		{
-			gw_log_print("DM",'E',"Job %i does not exist (JOB_STATE_PENDING).\n",job_id);
+        if ( job == NULL )
+        {
+            gw_log_print("DM",'E',"Job %i does not exist (JOB_STATE_PENDING).\n",job_id);
 
-			free(_job_id);
-			return;
-		}
-	}
-	else
-		return;
+            free(_job_id);
+            return;
+        }
+    }
+    else
+        return;
 
     /* ----------------------------------------------------------- */  
     /* 1.- Set state                                               */
@@ -60,7 +60,7 @@ void gw_dm_pending ( void *_job_id )
 
     gw_job_set_state (job, GW_JOB_STATE_PENDING, GW_FALSE);
     
-	/* -------- Update Host & User running jobs -------- */
+    /* -------- Update Host & User running jobs -------- */
 			            
     gw_user_pool_dec_running_jobs(job->user_id);
     
@@ -72,32 +72,27 @@ void gw_dm_pending ( void *_job_id )
 
     /* ------------- Notify the Scheduler --------------- */
    
-	if (job->history != NULL)
-	{	
+    if (job->history != NULL)
+    {	
         reason = GW_REASON_NONE;
 		    
-		failed =(job->history->reason == GW_REASON_EXECUTION_ERROR) ||
-                (job->history->reason == GW_REASON_PERFORMANCE);
-                                                     
+        failed = (job->history->reason == GW_REASON_EXECUTION_ERROR)
+                || (job->history->reason == GW_REASON_PERFORMANCE);
+ 
         if (failed)
         {
-            gw_dm_mad_job_failed(&gw_dm.dm_mad[0],
-	                         job->history->host->host_id,
-	                         job->user_id,
-	                         job->history->reason);
+            gw_dm_mad_job_failed(&gw_dm.dm_mad[0], job->history->host->host_id,
+	            job->user_id, job->history->reason);
         }
                               	                              
-        gw_dm_mad_job_schedule(&gw_dm.dm_mad[0],
-                           job_id,
-                           job->array_id,
-                           job->user_id,
-                           reason);
-	}
-	else
-    	gw_log_print("DM",'E',"Rescheduling job %d, but no history records found.\n", 
-    	    job->id);
+        gw_dm_mad_job_schedule(&gw_dm.dm_mad[0], job_id, job->array_id,
+                job->user_id, reason);
+    }
+    else
+        gw_log_print("DM",'E',"Rescheduling job %d, but no history records found.\n", 
+                job->id);
                            
-   /* ------------------------------------------------- */
+    /* ------------------------------------------------- */
 				        
     free(_job_id);
 		    
