f3708ac8ebea5467b44c8590481154c0e3b59e9b
ehuedo <ehuedo>
2012-05-16 10:28:18 +0000

    Bug #121: It takes too long to get CREAM and BES jobs cancelled
    git-svn-id: http://svn.gridway.org/gridway/branches/gw_5_10_branch@576 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/job/gw_history.c
+++ src/job/gw_history.c
@@ -120,8 +120,8 @@ int gw_job_history_add(gw_history_t **job_history,
     new_record->host         = host;
     new_record->tries        = 0;
     new_record->failed_polls = 0;
-    new_record->failed_cancels = 0;
-	new_record->counter      = -1;
+    new_record->cancel_tries = 0;
+    new_record->counter      = -1;
 
 	se = gw_host_get_genvar_str("SE_HOSTNAME", 0, host);
 
