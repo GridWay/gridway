9ca0ed31720dd8f6e8459dd7e3f4da6e3fc8c366
imarin <imarin>
2012-12-19 15:04:38 +0000

    Bug #149: Code cleaning and resource consumption improvements in BES driver
    git-svn-id: http://svn.gridway.org/gridway/trunk@675 9b4b2aa8-92f0-458a-8103-c3834d721760

--- src/em_mad/bes/GW_em_mad_bes.java
+++ src/em_mad/bes/GW_em_mad_bes.java
@@ -235,7 +235,7 @@ class GW_em_mad_bes extends Thread {
         void submit(int jid, String contact, String jsdl_file) {
 
 		String info = null;
-		int status = 0;
+		int status = -1;
 
                 // Submit the job
                 try
